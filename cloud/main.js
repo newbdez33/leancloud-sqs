bdd738b0c31822e8c4a5d7e86765310c027219bf
Jacky <newbdez33@gmail.com>
2015-01-13 23:29:02 +0800

    test2

--- cloud/main.js
+++ cloud/main.js
@@ -4,6 +4,20 @@ AV.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
 
+//用户钩子
 AV.Cloud.afterUpdate("_User", function(request) {
    console.log("Updated _user,the id is :" + request.object.id);
+});
+
+AV.Cloud.afterSave("_User", function(request) {
+  console.log(request.object);
+  // request.object.set("from","LeanCloud");
+  // request.object.save(null,{success:function(user)
+  //   {
+  //     console.log("ok!");
+  //   },error:function(user,error)
+  //   {
+  //     console.log("error",error);
+  //   }
+  //   });
 });
