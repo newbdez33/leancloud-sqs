d44954ed88f3083e826f520a5429528ad6476013
Jacky <newbdez33@gmail.com>
2015-01-13 23:15:05 +0800

    test

--- cloud/main.js
+++ cloud/main.js
@@ -4,7 +4,6 @@ AV.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
 
-AV.Cloud.afterSave("_User" function(request, response){
+AV.Cloud.afterSave("_User" function(request){
 	console.log("_User afterSave function is called");
-	response.success();
 });
