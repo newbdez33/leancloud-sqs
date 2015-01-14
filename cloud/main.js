bc1c96a3008145d5ddc6a1312aecaa335f898135
Jacky <newbdez33@gmail.com>
2015-01-14 22:26:15 +0800

    object log test

--- cloud/main.js
+++ cloud/main.js
@@ -1,12 +1,12 @@
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+  response.success("Hello from Jacky!");
 });
 
 //用户钩子
 AV.Cloud.afterUpdate("_User", function(request) {
-   console.log("Updated _user,the id is :" + request.object.id);
+   console.log("Updated _user:" + request.object);
 });
 
 AV.Cloud.afterSave("_User", function(request) {
